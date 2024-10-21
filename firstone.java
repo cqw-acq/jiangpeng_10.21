ba82465f29991ecf268850fbe6b6fce3bd859a50
cqw-acq <chuqijerry@outlook.com>
2024-10-21 17:34:38 +0800

    idk

--- firstone.java
+++ firstone.java
@@ -1,2 +1,7 @@
 public class firstone {
+    public static void main(String[] args) {
+        System.out.print("Today we are learning Java");//print 不换行：（
+        System.out.println("Today we are learning Java");//println 换行：）
+        System.out.println("Today we are learning Java");
+    }
 }
